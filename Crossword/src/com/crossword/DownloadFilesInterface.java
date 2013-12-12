028e6eeeaa39e65a2577f1ad9da1144692e20b93
qicaoyan111 <15210830240@139.com>
2013-12-12 21:32:06 +0800

    re

--- Crossword/src/com/crossword/DownloadFilesInterface.java
+++
@@ -1,7 +0,0 @@
-package com.crossword;
-
-public interface DownloadFilesInterface {
-    public void onDownloadTaskStarted();
-    public void onDownloadUpdateProgressStatus(String status);
-    public void onDownloadTaskCompleted(boolean completed, int progress, String errorMessage);
-}
